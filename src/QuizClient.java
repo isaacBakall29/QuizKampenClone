e0acb7912df1822963ed7663f0f069040de76976
Y3LL0W <isaacbakall29@gmail.com>
2024-11-15 05:00:32 +0100

    tcp client + server anslutning

---
+++ src/QuizClient.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.net.*;
+
+public class QuizClient {
+    public static void main(String[] args) {
+        String serverAddress = "localhost";
+        int port = 1333;
+
+        try (Socket socket = new Socket(serverAddress, port);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            
+            String initialMessage = in.readLine();
+            if (initialMessage != null) {
+                System.out.println("Server: " + initialMessage);
+            }
+
+            String userInputLine;
+            while ((userInputLine = userInput.readLine()) != null) {
+                out.println(userInputLine);
+                String response = in.readLine();
+                System.out.println("Server: " + response);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
